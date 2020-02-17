c609e13343b6b8cd80ffb75ab4280d541713482f
prajwaltelkar <55408935+prajwaltelkar@users.noreply.github.com>
2020-02-17 15:46:52 +0530

    Add files via upload

---
+++ C_files/func2.c
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "func2.h"
+using namespace std;
+ 
+void print()
+{
+cout<<"Hello World"<<endl;
+}
+
+
---
+++ C_files/main.c
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "func1.h"
+#include "func2.h"
+using namespace std;
+int main()
+{
+print();
+cout<<endl;
+cout<<"The factorial of 5 is"<<factorial(5)<<endl;
+return 0;
+}
